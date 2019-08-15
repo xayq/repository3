facbb307ccf43edc1df817c320a38a1627de1c62
xayq <xayq@126.com>
2019-08-15 17:40:49 +0800

    bbbbbbb

--- hello/src/com/yq/Test.java
+++ hello/src/com/yq/Test.java
@@ -3,5 +3,6 @@ package com.yq;
 public class Test {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbbbbbbbb");
     }
 }
